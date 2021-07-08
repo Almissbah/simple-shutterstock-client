bb00775f0e07ae0e43ad2b16dd135d24d535ed07
Mohammed Almissbah <mohammed.almissbah@hotmail.com>
2021-07-09 01:01:25 +0400

    add value equality

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,5 +1,10 @@
+import 'package:equatable/equatable.dart';
+
 /// Every failure retruned by the all repositories should be a subtype of this class.
-abstract class Failure {}
+abstract class Failure extends Equatable {
+   @override 
+  List<Object> get props => [];
+}
 
 /// Will be based in case of server failures.
 class ServerFailure extends Failure {}
