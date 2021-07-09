284e45c1790f3f99301aabe09dee7bc9d2ae58fe
Mohammed Almissbah <mohammed.almissbah@hotmail.com>
2021-07-09 17:22:20 +0400

    add failure messages.

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -2,12 +2,21 @@ import 'package:equatable/equatable.dart';
 
 /// Every failure retruned by the all repositories should be a subtype of this class.
 abstract class Failure extends Equatable {
-   @override 
-  List<Object> get props => [];
+  final String msg;
+
+  Failure(this.msg);
+  @override
+  List<Object> get props => [msg];
 }
 
 /// Will be based in case of server failures.
-class ServerFailure extends Failure {}
+class ServerFailure extends Failure {
+  ServerFailure() : super("Server Error, Please try again !");
+}
 
 /// Will be passed in case of network failures.
-class NoConnectionFailure extends Failure {}
+class NoConnectionFailure extends Failure {
+  NoConnectionFailure()
+      : super(
+            "Failed to connect, please check your internet connection and try again !");
+}
